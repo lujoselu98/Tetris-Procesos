7ed40a021dc3314e3b2c899d402bab74aa018794
AlexCh98 <alexcheca.98@gmail.com>
2019-11-13 14:28:52 +0100

    Camara terminada

--- app/src/main/java/com/example/activities/CameraActivity.java
+++ app/src/main/java/com/example/activities/CameraActivity.java
@@ -6,34 +6,55 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 public class CameraActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
-
+    private Bundle datos;
+    private Bitmap photo;
+    private Intent intent;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acticity_camera);
+        setContentView(R.layout.activity_camera);
+        datos = this.getIntent().getExtras();
         this.imageView = (ImageView) this.findViewById(R.id.imageView);
         Button photoButton = (Button) this.findViewById(R.id.button);
-        photoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-                } else {
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                }
+        photoButton.setOnClickListener(v -> {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            } else {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
+        });
+        Button botonContinuar = findViewById(R.id.botonContinuar);
+        botonContinuar.setOnClickListener(v -> {
+                if(photo!=null){
+                    Intent intent = new Intent(this, PantallaReinicio.class);
+                    intent.putExtras(datos);
+                    intent.putExtra("fotoUsuario", photo);
+                    this.startActivity(intent);
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("TIENES QUE HACERTE UNA FOTO PARA CONTINUAR")
+                            .setTitle("")
+                            .setCancelable(false)
+                            .setNeutralButton("PERFECTO",
+                                    (dialog, id) -> {
+                                        dialog.cancel();
+                                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+
         });
     }
 
@@ -54,7 +75,7 @@ public class CameraActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
         }
     }
